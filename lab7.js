aacc145af5cf0d13e533865f9d49ea5f7ef06c5d
crhidrogo <claudiahidrogo+github@gmail.com>
2021-11-08 02:45:47 -0800

    added timer button, but no functionality. TODO: timer func

--- lab7.js
+++ lab7.js
@@ -1,7 +1,10 @@
-const difficulties = ["Easy", "Medium", "Hard"];
+const difficulties = ["Easy","Pretty Easy", "Medium", "Medium-ish", "Hard", "Hella Hard"];
 const goalString = ["lad fad sad dad",
+  "dog tog log bog",
   "This is not a test.",
-  "True! --nervous --very, very dreadfully nervous I had been and am; but why will you say that I am mad?"];
+  "So, I was heading to this place, but I couldn't remember the name..",
+  "True! --nervous --very, very dreadfully nervous I had been and am; but why will you say that I am mad?",
+  "Grow old along with me! The best is yet to be, the last of life, for which the first was made. Our times are in his hand who saith, 'A whole I planned, youth shows but half; Trust God: See all, nor be afraid!"];
 
 var i = 0;
 
@@ -12,10 +15,45 @@ window.addEventListener("keypress", function (event) {
     const result = document.getElementById("result");
     result.textContent = "You Win!";
     textbox.textContent = "";
-    i = i+1;
-    lvl = document.getElementById("level")
-    lvl.textContent = difficulties[i]
-    goal = document.getElementById("goal")
-    goal.textContent = goalString[i]
-  }
-})
+
+    //create Next button
+    next = document.createElement('button')
+    document.body.appendChild(next)
+    next.textContent = "Next"
+    next.addEventListener('click', nextChallenge)
+
+    // create Next button functionality
+    function nextChallenge (){
+      i = i+1;
+      lvl = document.getElementById("level")
+      lvl.textContent = difficulties[i]
+      goal = document.getElementById("goal")
+      goal.textContent = goalString[i]
+    }
+  
+  } // end of if-statement 
+  restartButton = document.querySelector("#restartButton")
+  restartButton.addEventListener('click', restart)
+
+  // create timer
+  
+}); // end of window listening
+
+
+
+function restart() {
+  // In here set i=0
+  let i = 0;
+  // find the level, result, and goal elements and reset their textContent
+  lvl = document.getElementById("level")
+  lvl.textContent = difficulties[i];
+  goal = document.getElementById("goal")
+  goal.textContent = goalString[i];
+  result.textContent = "Type the Following text:";
+  textbox = document.getElementById("userinput");
+  textbox.textContent = ""; // after reset when user enters space, entire string is set to ""??
+
+};
+
+
+
