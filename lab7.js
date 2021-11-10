700bcbddc531a415c48e6586379ed3d2886cefc5
crhidrogo <claudiahidrogo+github@gmail.com>
2021-11-09 20:23:00 -0800

    fixed creation of multiple next buttons and added div to html

--- lab7.js
+++ lab7.js
@@ -7,53 +7,83 @@ const goalString = ["lad fad sad dad",
   "Grow old along with me! The best is yet to be, the last of life, for which the first was made. Our times are in his hand who saith, 'A whole I planned, youth shows but half; Trust God: See all, nor be afraid!"];
 
 var i = 0;
+let flag = false; // flag for existance of Next button
 
 window.addEventListener("keypress", function (event) {
   const textbox = document.getElementById("userinput");
   textbox.textContent = textbox.textContent + event.key;
+  
   if (textbox.textContent == goalString[i]) {
     const result = document.getElementById("result");
     result.textContent = "You Win!";
     textbox.textContent = "";
 
-    //create Next button
-    next = document.createElement('button')
-    document.body.appendChild(next)
-    next.textContent = "Next"
-    next.addEventListener('click', nextChallenge)
-
-    // create Next button functionality
-    function nextChallenge (){
-      i = i+1;
-      lvl = document.getElementById("level")
-      lvl.textContent = difficulties[i]
-      goal = document.getElementById("goal")
-      goal.textContent = goalString[i]
+    //create Next button (if it doesn't already exist)
+    if (!flag) {
+      next = document.createElement('button');
+      flag = true;
+      next.textContent = "Next";
+      next.addEventListener('click', nextChallenge);
+      document.body.appendChild(next);
     }
   
-  } // end of if-statement 
-  restartButton = document.querySelector("#restartButton")
-  restartButton.addEventListener('click', restart)
-
-  // create timer
+  } // end of correct user input statement 
   
 }); // end of window listening
 
-
+// create Restart button
+restartButton = document.querySelector("#restartButton");
+restartButton.addEventListener('click', restart);
 
 function restart() {
   // In here set i=0
-  let i = 0;
+  i = 0;
   // find the level, result, and goal elements and reset their textContent
-  lvl = document.getElementById("level")
+  lvl = document.getElementById("level");
   lvl.textContent = difficulties[i];
-  goal = document.getElementById("goal")
+  goal = document.getElementById("goal");
   goal.textContent = goalString[i];
   result.textContent = "Type the Following text:";
   textbox = document.getElementById("userinput");
   textbox.textContent = ""; // after reset when user enters space, entire string is set to ""??
+  document.body.removeChild(next);
+  flag = false;
 
-};
+  // timer
+  if (typeof start != 'undefined'){
+    start = 0;
+    document.body.removeChild(timing);
+  }
+}
+
+// create Next button functionality
+function nextChallenge (){
+  i = i+1;
+  result.textContent = "Type the Following text:";
+  lvl = document.getElementById("level");
+  lvl.textContent = difficulties[i];
+  goal = document.getElementById("goal");
+  goal.textContent = goalString[i];
+  textbox = document.getElementById("userinput");
+  textbox.textContent = "";
+
+  //timer
+  if (typeof start != 'undefined') {
+    end = Date.now();
+    diff = (end - start)/1000;
+    timing = document.createElement('p');
+    timing.textContent = diff;
+    document.body.appendChild(timing);
+  }
+  
+}
+
+// create timer functionality
+timerButton = document.querySelector("#timerButton")
+timerButton.addEventListener('click', function(){
+  start = Date.now();
+  console.log(start);
+})
 
 
 
